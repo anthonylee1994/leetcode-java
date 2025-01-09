360605c4a55e96752733077382314ac0766c6af0
Anthony Lee <anthonylee1994@users.noreply.github.com>
2025-01-09 10:00:21 +0800

    init

---
+++ src/main/java/com/learn/ConcatenationOfArray.java
@@ -0,0 +1,25 @@
+package com.learn;
+
+/**
+ * @author anthonylee
+ */
+public class ConcatenationOfArray {
+    public int[] getConcatenation(int[] nums) {
+        int[] result = new int[nums.length * 2];
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = nums[i];
+            result[i + nums.length] = nums[i];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ConcatenationOfArray concatenationOfArray = new ConcatenationOfArray();
+        int[] nums = {1, 2, 1};
+        int[] result = concatenationOfArray.getConcatenation(nums);
+
+        for (int j : result) {
+            System.out.println(j);
+        }
+    }
+}
